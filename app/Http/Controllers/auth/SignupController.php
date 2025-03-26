c49bf7b90373575ecffbd482084973265f3c8bae
ThiagoGuilherme71 <thiagoguilherme.barbosaa@gmail.com>
2025-03-26 10:12:16 -0300

    alterações signUp-guia

--- app/Http/Controllers/auth/SignupController.php
+++ app/Http/Controllers/auth/SignupController.php
@@ -49,7 +49,40 @@ class SignupController extends Controller
 
     public function signupGuia(Request $request)
     {
-        // Lógica para processar o cadastro do guia
+        $request->validate([
+            'nome' => 'required',
+            'email' => 'required|email|unique:guias,email',
+            'telefone' => 'required|string|max:18',
+            'data_nascimento' => 'required|date',
+            'cpf' => 'required|string|unique:guias,cpf',
+            'cep' => 'required|string|unique:guias,cep',
+            'endereco' => 'required',
+            'link_instagram' => 'required',
+            'link_facebook' => 'required',
+            'doc_frente' => 'required',
+            'doc_verso' => 'required',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+
+        $guia = Guia::create([
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'telefone' => $request->telefone,
+            'data_nascimento' => $request->data_nascimento,
+            'cpf' => $request->cpf,
+            'cep' => $request->cep,
+            'endereco' => $request->endereco,
+            'link_instagram' => $request->link_instagram,
+            'link_facebook' => $request->link_facebook,
+            'doc_frente' => $request->doc_frente,
+            'doc_verso' => $request->doc_verso,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->login($guia);
+        // tem que mudar essa view e criar um dash do guia
+        return redirect()->route('landing-page')->with('success', 'Cadastro realizado com sucesso!');
     }
 
 }
